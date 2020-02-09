24c340e81bf7d12281261ae925d705a986bd82a2
Tariqul Alam <tariqul2814@gmail.com>
2020-02-09 15:28:38 +0600

    First Commit

---
+++ VidlyTutorial/Controllers/CustomerController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using VidlyTutorial.Models;
+
+namespace VidlyTutorial.Controllers
+{
+    public class CustomerController : Controller
+    {
+        ApplicationDbContext context;
+        public CustomerController()
+        {
+            context = new ApplicationDbContext();
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            context.Dispose();
+        }
+        // GET: Customer
+        public ActionResult Index()
+        {
+            var customers = context.customers.Include(c=> c.MembershipType).ToList();
+            return View(customers);
+        }
+        
+        public ActionResult Details(int id)
+        {
+            var customer = context.customers.Include(c => c.MembershipType).SingleOrDefault(c => c.Id == id);
+            if (customer == null)
+                return HttpNotFound();
+            return View(customer);
+        }
+    }
+}
---
+++ VidlyTutorial/Controllers/MovieController.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using VidlyTutorial.Models;
+using VidlyTutorial.Models.ViewModel;
+
+namespace VidlyTutorial.Controllers
+{
+    public class MovieController : Controller
+    {
+        ApplicationDbContext context = new ApplicationDbContext();
+        // GET: Movie
+        [HttpGet]
+        public ActionResult Random()
+        {
+            var movie = new Movie()
+            {
+                Id = 1,
+                Name = "Shrek!"
+            };
+            var customers = new List<Customer> 
+            {
+                new Customer {Name="Customer 1"},
+                new Customer {Name ="Customer 2"}
+            };
+            var viewModel = new RandomMovieViewModel
+            {
+                Movie = movie,
+                Customers = customers
+            };
+            return View(viewModel);
+        }
+
+        public ActionResult Movie()
+        {
+            var Movies = context.movies.ToList();
+            //var customers = context.customers.Include(c=> c.MembershipType).ToList();
+            return View(Movies);
+        }
+
+        public ActionResult Edit(int? id, string name)
+        {
+            return Content("Hello "+name+" ("+id+")");
+        }
+
+        public ActionResult Details(int id)
+        {
+            return Content("");
+        }
+
+        public ActionResult Index (int? pageIndex, string sortBy)
+        {
+            if(!pageIndex.HasValue)
+            {
+                pageIndex = 1;
+            }
+            if (string.IsNullOrWhiteSpace(sortBy))
+                sortBy = "Name";
+
+            return Content(string.Format("pageIndex = {0} & sortBy={1}",pageIndex,sortBy));
+        }
+
+        [Route("movie/released/{year}/{month:regex(\\d{2}):range(1,12)}")]
+        //[Route("movie/released/{year}/{month}")]
+        //[Route(“books/{isbn?}”)]
+        public ActionResult ByReleaseYear(int year, int month)
+        {
+            return Content(year+"/"+month);
+        }
+    }
+}
